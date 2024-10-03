c4f7a18aa0bd21fd0057f046de1a80a0620b3255
MaviaBet <betmavia@gmail.com>
2024-10-03 15:53:03 -0400

    Add files via upload

--- deposit/script.js
+++ deposit/script.js
@@ -1,6 +1,56 @@
-const walletAddress = "0xC6755Bf72Eb0F376B990327ab8b94fE2730f4de7"; // Dirección de depósito
+let walletAddress; // Dirección de depósito
 const contractAddress = "0x24fcFC492C1393274B6bcd568ac9e225BEc93584"; // Dirección del contrato del token Mavia en la red Base
 const decimals = 18; // Decimales del token Mavia (puede variar, pero comúnmente es 18)
+let chatId;
+
+// Acceder a los datos del usuario proporcionados por Telegram
+// Comprobar si window.Telegram.WebApp está definido
+if (window.Telegram && window.Telegram.WebApp) {
+window.Telegram.WebApp.ready(function() {
+    // Obtener los datos del chat_id del usuario
+    const initData = window.Telegram.WebApp.initDataUnsafe;
+    chatId = initData.chat.id;  // Aquí tienes el chat_id del usuario
+    // Obtener la dirección de la wallet desde la URL
+    const params = new URLSearchParams(window.location.search);
+  walletAddress = params.get("wallet_address");
+
+    // Verificar si el chat_id es null
+            let formattedChatId;
+            if (chatId === null) {
+                formattedChatId = "Chat ID not available [A]";  // Mensaje alternativo
+            } else {
+                // Formatear el chat_id a "123...789"
+                formattedChatId = formatChatId(chatId);
+            }
+
+    // Mostrar el chat_id formateado al lado de "User : "
+    const chatIdElement = document.getElementById("chat_id");
+    chatIdElement.textContent = "User : " + formattedChatId;
+
+});
+}else{
+    // Manejo del caso en que window.Telegram.WebApp no esté disponible
+    const chatIdElement = document.getElementById("chat_id");
+    chatIdElement.textContent = "Telegram WebApp not available";
+    showAlertAndClose();  // Mostrar alerta y cerrar la ventana
+}
+
+// Función para mostrar un diálogo y cerrar la ventana
+function showAlertAndClose() {
+    alert("Chat ID is not available. It is not possible to make the deposit.");
+    //TODO ELIMINAR ESTA LINEA DE CODIGO PARA PRUEBAS
+    //window.close();  // Cierra la ventana
+}
+
+// Función para formatear el chat_id
+function formatChatId(id) {
+    const idString = id.toString();  // Convertir a cadena
+    if (idString.length <= 6) {
+        return idString;  // Si el ID es corto, retornar tal cual
+    }
+    // Retornar solo los primeros 3 y últimos 3 dígitos
+    return idString.substring(0, 3) + '...' + idString.substring(idString.length - 3);
+}
 
 function toTokenAmount(value, decimals) {
     return (value * Math.pow(10, decimals)).toString();
@@ -8,8 +58,8 @@ function toTokenAmount(value, decimals) {
 
 function updateQR() {
     let maviaAmount = document.getElementById('maviaAmount').value;
-    let rubies = (maviaAmount * 100).toFixed(2); // Redondea a 2 decimales
-    document.getElementById('rubiesAmount').textContent = rubies; // Actualiza el valor de rubíes
+    let rubi = (maviaAmount * 100).toFixed(2); // Redondea a 2 decimales
+    document.getElementById('rubiesAmount').textContent = rubi; // Actualiza el valor de rubíes
 
     if (maviaAmount > 0) {
         let tokenAmount = toTokenAmount(maviaAmount, decimals);
@@ -58,3 +108,13 @@ window.onclick = function(event) {
         closeModal();
     }
 }
+
+function openLink(){
+
+}
+
+
+
+
+
+
