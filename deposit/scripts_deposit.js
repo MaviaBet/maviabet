aae81563ca78f4d4ae3b00361bbcaca13b920841
MaviaBet <betmavia@gmail.com>
2024-10-17 20:10:57 -0400

    Add files via upload

--- deposit/scripts_deposit.js
+++ deposit/scripts_deposit.js
@@ -4,8 +4,16 @@ const decimals = 18; // Decimales del token Mavia (puede variar, pero comúnment
 let chatId;
 let password;
 const updateMaviaAlerta="https://script.google.com/macros/s/AKfycbwK5ShC90SpgxHmLVDpNpuEjlHPCnU8WWFrM6yTiQHhbm-znekISUE-IsUQ7f4dt6f_dg/exec";
+const getMaviaAlerta="https://script.google.com/macros/s/AKfycbzE8pu8S0CZEW8vG-drlUNZ-K2g_DR4hTfAqmOpl6JHFwvz74O6RhSrRvDb6LB_56uJ7Q/exec";
 // Define la versión
 const version = "v1.0.0"; // Cambia este valor cuando necesites actualizar la versión
+
+let alertDialog ;
+let progressBarInner ;
+let progressText ;
+let overlay;
+let percentage = 0;
+
 
 //chat_id=1234567&wallet_address=0x123456789012345678
 // Llamar a la función initialize_Deposit al cargar la página
@@ -27,6 +35,13 @@ async function initialize_Deposit() {
     walletAddress = params.get("wallet_address");
     password=params.get("password");
 ///////////////////////////////////////////////////
+    alertDialog = document.getElementById('alert-dialog');
+    progressBarInner = document.querySelector('.progress-bar-inner');
+progressText = document.getElementById('progress-text');
+overlay = document.getElementById('overlay');
+percentage = 0;
+
+
     let formattedWalletAddress;
     const chatIdElement = document.getElementById("chat_id");
     const walletAddressElement = document.getElementById("wallet_Address");
@@ -99,9 +114,30 @@ document.getElementById('maviaAmount').addEventListener('input', () => {
 document.getElementById('maviaAmount').addEventListener('change', updateQR_Deposit); // Para móviles
 
 
-function generateQR_Deposit() {
+async function generateQR_Deposit() {
+let maviaAmount = document.getElementById('maviaAmount').value;
+if(maviaAmount===''){
+    return;
+}
+let valorNumerico = parseFloat(maviaAmount);
+
+if (!isNaN(valorNumerico) && valorNumerico === 0) {
+  console.log("El valor es 0");
+} else {
     const qrData = updateQR_Deposit(); // Asegúrate de que se genera el QR
-    showModal_Deposit(qrData); // Pasa el qrData al modal
+    showDialog_deposit();
+    updateProgressBar_deposit(50); // Update the progress bar to 50%
+    //Comprobar si hay un deposito en proceso
+    const depositoPendiente=await getMaviaAlerta_Deposit(chatId,password);
+    updateProgressBar_deposit(100); // Update the progress bar to 100%
+    closeDialog_deposit();
+
+        if(depositoPendiente==='0'){
+            showModal_Deposit(qrData); // Pasa el qrData al modal
+        }else{
+            showModalAlert_deposit('Your deposit cannot be processed at this time, please try again in a few minutes');
+        }
+}
 }
 
 
@@ -130,31 +166,71 @@ window.onclick = function(event) {
 async function updateMaviaAlerta_Deposit(charId, newMavia) {
     // URL del Web App con parámetros char_id y new_mavia_alerta
     const url = `${updateMaviaAlerta}?action=updateMaviaAlerta&char_id=${charId}&new_mavia_alerta=${newMavia}&password=${password}`;
-
-    try {
-        // Realizar la solicitud GET
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Accept': 'text/plain' // O 'application/json' si el servidor devuelve JSON
-            }
-        });
-
-        // Comprobar si la solicitud fue exitosa (código 200)
-        if (response.ok) {
-            // Leer y devolver el contenido de la respuesta
-             // O .json() si es JSON
-            return await response.text();
-        } else {
-            // Manejar el error de la solicitud
-            console.error(`Error en la solicitud GET. Código de estado: ${response.status}`);
-            return `Error en la solicitud GET. Código de estado: ${response.status}`;
-        }
-    } catch (error) {
-        // Manejar errores de red o de conexión
-        console.error('Error al actualizar Mavia:', error);
-        return `Error al actualizar Mavia: ${error}`;
-    }
+    return await load_url_Deposit(url);
 }
 
 
+
+async function getMaviaAlerta_Deposit(charId,password) {
+        const url = `${getMaviaAlerta}?action=getMaviaAlerta&char_id=${charId}&password=${password}`;
+       return await load_url_Deposit(url);
+    }
+
+
+    async function load_url_Deposit(url){
+ try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'text/plain', // O 'application/json' si es JSON
+                },
+            });
+            if (!response.ok) {
+                console.error(`HTTP error! Status : ${response.status}`);
+                return `HTTP error! Status : ${response.status}`;
+            }
+            // O .json() si es JSON
+            return await response.text();
+        } catch (error) {
+            console.error('Error fetching :', error);
+            return 'Error fetching :'+error;
+        }
+    }
+
+
+
+
+ function showModalAlert_deposit(message) {
+    const modal = document.getElementById("errorModal");
+    const modalMessage = document.getElementById("modalMessage");
+    modalMessage.textContent = message; // Cambia el texto del mensaje
+    modal.style.display = "block"; // Mostrar el modal
+}
+
+
+function closeModalAlert_deposit() {
+    const modal = document.getElementById("errorModal");
+    modal.style.display = "none"; // Ocultar el modal
+}
+
+
+
+
+
+// Function to show the dialog
+function showDialog_deposit(){
+    overlay.style.display = 'block'; /* Muestra el overlay */
+    alertDialog.style.display = 'block'; /* Muestra la barra de progreso */
+}
+
+// Function to close the dialog
+function closeDialog_deposit() {
+    overlay.style.display = 'none'; /* Oculta el overlay */
+    alertDialog.style.display = 'none'; /* Oculta la barra de progreso */
+}
+
+// Function to update the progress bar
+function updateProgressBar_deposit(percentage) {
+progressBarInner.style.width = `${percentage}%`;
+progressText.textContent = `${percentage}% Complete`;
+}
